22f37cdca4cdfba781f2ce0bf0aba490166bc97e
Pázmányi Sándor <pazmanyi.sandor2004@gmail.com>
2024-11-25 18:05:22 +0100

    initted repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ekozigPersonEntryDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
